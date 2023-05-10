57262776ce54e7b33ecce854c1dfed3d7e26506a
Pranjal Chaurasiya <92513144+Its-Pranjal@users.noreply.github.com>
2023-05-10 11:45:03 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
    loader(messageDiv);
 
 //fetch data from server through bot's response
-   const response = await fetch('http://localhost:5000', {
+   const response = await fetch('https://backend-6s5m.onrender.com/', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
